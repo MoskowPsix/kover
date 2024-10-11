88820266f004c25661988e0fbfa37c376f48b2cd
MoskowPsix <bolshe.kivi@gmail.com>
2024-10-11 22:04:46 +0500

    add token

--- request_hack.py
+++ request_hack.py
@@ -5,8 +5,9 @@ from dotenv import dotenv_values
 class RequestHack:
 
     url = dotenv_values('.env').get('APP_URL')
+    token = dotenv_values('.env').get('TOKEN')
 
     def post(self, params: object): 
-        return requests.post(f'{self.url}/play/magcarp/player/move', params).json
+        return requests.post(f'{self.url}/play/magcarp/player/move', params, headers=f'X-Auth-Token={self.token}').json
     def get(self):
         return requests.get(f'{self.url}/rounds/magcarp').json()
