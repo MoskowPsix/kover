3760abe8754831ffd7b089f448b1d92411a8d790
Ada-lave <mega.kefi36@gmail.com>
2024-10-11 14:16:13 -0400

    draw maps

--- map.py
+++ map.py
@@ -1,18 +1,66 @@
 import matplotlib.pyplot as plt
 import requests
+import time
+from request_hack import RequestHack
 
 
 AREA_SIZE = 9000
 URL = " https://games-test.datsteam.dev"
+fig, ax = plt.subplots()
+
+
+req_hack = RequestHack()
+
 
 def get_map():
-    data = requests.post(URL + "/play/magcarp/player/move", {"transports": []}, headers={"X-Auth-Token": "6703e10e0883d6703e10e08840"}).json()
+    data = req_hack.post(params={"transports": []})
     return data
 
+
 def get_anomalies(data):
-       anomalies = data['anomalies']
-       print(anomalies)
 
-data = get_map()
+    anomalies = data["anomalies"]
+    for obj in anomalies:
+        circle = plt.Circle(
+            (obj["x"], obj["y"]), obj["radius"], color="blue", fill=True, alpha=0.5
+        )
+        ax.add_artist(circle)
 
-get_anomalies(data)
+
+def get_our_kovrs(data):
+    kovrs = data["transports"]
+    for obj in kovrs:
+        circle = plt.Circle((obj["x"], obj["y"]), 50, color="red", fill=True, alpha=0.5)
+        ax.add_artist(circle)
+
+
+def get_enemies(data):
+    enemies = data["enemies"]
+    for obj in enemies:
+        circle = plt.Circle(
+            (obj["x"], obj["y"]), 50, color="black", fill=True, alpha=0.5
+        )
+        ax.add_artist(circle)
+
+
+while True:
+    ax.cla()
+
+    # Задаем размеры области
+    ax.set_xlim(0, AREA_SIZE)
+    ax.set_ylim(0, AREA_SIZE)
+
+    data = get_map()
+    get_anomalies(data)
+    get_our_kovrs(data)
+    get_enemies(data)
+
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+    # Сохраняем соотношение сторон
+    ax.set_aspect("equal", adjustable="box")
+
+    # Показываем график
+    plt.savefig("out.png")
+    time.sleep(1)
--- request_hack.py
+++ request_hack.py
@@ -4,11 +4,15 @@ from dotenv import dotenv_values
 
 class RequestHack:
 
-    url = dotenv_values('.env').get('APP_URL')
-    token = dotenv_values('.env').get('TOKEN')
+    url = dotenv_values(".env").get("APP_URL")
+    token = dotenv_values(".env").get("TOKEN")
 
-    def post(self, params): 
-        header = {'X-Auth-Token': self.token}
-        return requests.post(f'{self.url}/play/magcarp/player/move',data=params, headers=header).json()
+    def post(self, params=[]):
+        return requests.post(
+            f"{self.url}/play/magcarp/player/move",
+            params,
+            headers={"X-Auth-Token": self.token},
+        ).json()
+
     def get(self):
-        return requests.get(f'{self.url}/rounds/magcarp').json()
+        return requests.get(f"{self.url}/rounds/magcarp").json()
